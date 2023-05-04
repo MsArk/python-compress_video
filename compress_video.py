fb7d12ab1af0fa98c031796c479975bb5dbcdfa0
Ivan <ivjaehnke@gmail.com>
2023-05-04 15:14:46 +0200

    Add env file

--- compress_video.py
+++ compress_video.py
@@ -1,6 +1,12 @@
-# pip install moviepy
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 import moviepy.editor as mp
 
-clip = mp.VideoFileClip("VIDEO_INPUT.MP4")
+INPUT_FILE = os.getenv('INPUT_FILE')
+OUTPUT_FILE = os.getenv('OUTPUT_FILE')
+
+clip = mp.VideoFileClip(INPUT_FILE)
 clip_resized = clip.resize(height = 1080)
-clip_resized.write_videofile("VIDEO_INPUT_Resized.mp4")
+clip_resized.write_videofile(OUTPUT_FILE)
